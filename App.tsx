c91d81c57a4545e642a7bcb98de7857d05f994ea
Mélissa Kintz <kntzmelissa@gmail.com>
2022-02-17 11:02:40 +0100

    fix error bundle

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, AppRegistry } from 'react-native';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 
 // Initialize Apollo Client
 const client = new ApolloClient({
@@ -9,14 +8,13 @@ const client = new ApolloClient({
   cache: new InMemoryCache()
 });
 
-console.log(client)
 
 export default function App() {
 
   return (
     <ApolloProvider client={client}>
       <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
+        <Text>Open up App.tsx to start working on your app :):):):):)!</Text>
         <StatusBar style="auto" />
       </View>
     </ApolloProvider>
---
+++ metro.config.js
@@ -0,0 +1,7 @@
+import { getDefaultConfig } from "@expo/metro-config";
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+defaultConfig.resolver.sourceExts.push("cjs");
+
+export default defaultConfig;
