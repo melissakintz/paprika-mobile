fa03321ff4e1829d390e6317e31ce99bacd0b45e
lolarne <rne.lola@gmail.com>
2022-02-17 11:55:14 +0100

    add navigation bottom bar with project, task and home page + icons

--- App.tsx
+++ App.tsx
@@ -1,6 +1,15 @@
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { StatusBar } from "expo-status-bar";
 import { AppRegistry, StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+import ProjectScreen from "./screens/ProjectScreen";
+import TaskScreen from "./screens/TaskScreen";
+import HomeScreen from "./screens/HomeScreen";
+
+const Tab = createBottomTabNavigator();
 
 // Initialize Apollo Client
 const client = new ApolloClient({
@@ -11,21 +20,39 @@ const client = new ApolloClient({
 export default function App() {
   return (
     <ApolloProvider client={client}>
-      <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app :):):):):)!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+
+              if (route.name === "Projets") {
+                iconName = focused ? "briefcase" : "briefcase-outline";
+              } else if (route.name === "Accueil") {
+                iconName = focused ? "fast-food" : "fast-food-outline";
+              } else if (route.name === "Tâches") {
+                iconName = focused
+                  ? "checkmark-circle"
+                  : "checkmark-circle-outline";
+              }
+
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+
+            tabBarActiveTintColor: "#E33636",
+
+            tabBarInactiveTintColor: "gray",
+          })}
+        >
+          <Tab.Screen name="Projets" component={ProjectScreen} />
+          <Tab.Screen name="Accueil" component={HomeScreen} />
+          <Tab.Screen name="Tâches" component={TaskScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
     </ApolloProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const styles = StyleSheet.create({});
 
 AppRegistry.registerComponent("Paprika", () => App);
---
+++ screens/TaskScreen.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import {Text} from 'react-native';
+
+export default function TaskScreen(){
+    return (
+        <Text>Tâches</Text>
+    )
+}
