18366da929d834e776eb5a0fc209be10a859549d
lolarne <rne.lola@gmail.com>
2022-02-18 11:09:56 +0100

    add task list

--- App.tsx
+++ App.tsx
@@ -13,7 +13,7 @@ const Tab = createBottomTabNavigator();
 
 // Initialize Apollo Client
 const client = new ApolloClient({
-  uri: "localhost:4000/graphql",
+  uri: "http:/192.168.1.20:4000/graphql",
   cache: new InMemoryCache(),
 });
 
