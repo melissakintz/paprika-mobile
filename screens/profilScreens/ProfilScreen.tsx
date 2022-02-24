5a72dd6dff06781c977fccb6872987caa34eb2d9
hfx <hemmerfrancoisxavier@gmail.com>
2022-02-24 15:54:04 +0100

    add some file for refactoring

--- screens/profilScreens/ProfilScreen.tsx
+++ screens/profilScreens/ProfilScreen.tsx
@@ -1,15 +1,69 @@
-import { Text, View } from "react-native";
+import { FlatList, View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import TaskProgress from "./TaskProgress";
+import NameCurrent from "./NameCurrent";
+import LastNameCurrent from "./LastNameCurrent";
+import EmailCurrent from "./EmailCurrent";
+import RoleCurrent from "./RoleCurrent";
 
 export default function ProfilScreen({ route }: object) {
   const { user } = route.params;
   const currentUser = user.getUser;
   return (
-    <View>
-      <Text>Nom: {currentUser.firstName }</Text>
-      <Text>Prénom: {currentUser.lastName }</Text>
-      <Text>Email:{currentUser.email }</Text>
-      <Text>Rôle:{currentUser.role}</Text>
-      <Text>Tâche(s) en cours:{currentUser.role}</Text>
+    <View style={styles.containerBig}>
+      <View style={styles.container}>
+        <NameCurrent />
+        <LastNameCurrent />
+        <EmailCurrent />
+        <RoleCurrent />
+      </View>
+    <TaskProgress />
+
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  containerBig: {
+    backgroundColor: "white",
+    flex: 1,
+    padding: 20
+  },
+  container: {
+    backgroundColor: "#e6dcdc",
+    justifyContent: "space-between",
+    borderRadius: 5,
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+    borderRightColor: "black",
+    borderRightWidth: 1
+  },
+  containerList: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 10
+  },
+  textAlignLeft: {
+    textAlign: "left",
+    justifyContent: "flex-start"
+  },
+  textAlignRight: {
+    textAlign: "right",
+    justifyContent: "flex-end"
+  },
+  containerTask: {
+    paddingTop: 30
+  },
+  textCenter: {
+    textAlign: "center",
+    textDecorationLine: "underline",
+    paddingBottom: 20,
+  },
+  textSize: {
+    fontSize: 20
+  },
+  textUnderline: {
+    textDecorationLine: "underline",
+  }
+});
---
+++ screens/profilScreens/RoleCurrent.tsx
@@ -0,0 +1,66 @@
+import { FlatList, View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { useGetUserQuery, useGetAllUsersQuery } from "../../graphql/graphql";
+
+export default function RoleCurrent() {
+  const currentUserTab = useGetAllUsersQuery();
+  const currentUser = currentUserTab.data?.getAllUsers[0].id;
+  const { data: user, error: errorUser } = useGetUserQuery({
+      variables: { userId: currentUser! },
+  });
+    return (
+        <View style={styles.containerList}>
+            <View>
+                <Text style={[styles.textAlignLeft, styles.textSize , styles.textUnderline]}>Rôles: </Text>
+            </View>
+            <View>
+                <Text style={styles.textAlignRight}>{currentUserTab.data?.getAllUsers[0].role}</Text>
+            </View>
+        </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+    containerBig: {
+      backgroundColor: "white",
+      flex: 1,
+      padding: 20
+    },
+    container: {
+      backgroundColor: "#e6dcdc",
+      justifyContent: "space-between",
+      borderRadius: 5,
+      borderBottomColor: "black",
+      borderBottomWidth: 1,
+      borderRightColor: "black",
+      borderRightWidth: 1
+    },
+    containerList: {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-between",
+      padding: 10
+    },
+    textAlignLeft: {
+      textAlign: "left",
+      justifyContent: "flex-start"
+    },
+    textAlignRight: {
+      textAlign: "right",
+      justifyContent: "flex-end"
+    },
+    containerTask: {
+      paddingTop: 30
+    },
+    textCenter: {
+      textAlign: "center",
+      textDecorationLine: "underline",
+      paddingBottom: 20,
+    },
+    textSize: {
+      fontSize: 20
+    },
+    textUnderline: {
+      textDecorationLine: "underline",
+    }
+  });
