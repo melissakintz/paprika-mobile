76c42c7ac2cd377e135137f697a40ebc9f66bdeb
HFX <hemmerfrancoisxavier@gmail.com>
2022-04-01 11:54:40 +0200

    [UPDATE] - after pull dev

--- screens/profilScreens/ProfilScreen.tsx
+++ screens/profilScreens/ProfilScreen.tsx
@@ -1,27 +1,23 @@
 import { FlatList, View, Text, StyleSheet, TouchableOpacity } from "react-native";
-import { useGetUserQuery, useGetAllUsersQuery, useGetProjectRolesQuery } from "../../graphql/graphql";
+import { useGetUserQuery, useGetAllUsersQuery, useGetCurrentUserQuery } from "../../graphql/graphql";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import TaskProgress from "./TaskProgress";
 import InformationsUser from "./InformationsUser";
+import getUser from "../../utils/userUtils";
 
 export default function ProfilScreen({ route }: object) {
   const { user } = route.params;
-  const currentUser = user.getCurrentUser();
-  const {
-    data: roleUser,
-    error,
-  } = useGetProjectRolesQuery();
+  const currentUser = getUser.getCurrentUser();
 
   return (
     <View style={styles.containerBig}>
       <View style={styles.container}>
-        <InformationsUser name={"Nom"} information={currentUser.firstName}/>
-        <InformationsUser name={"Prénom"} information={currentUser.lastName}/>
-        <InformationsUser name={"Email"} information={currentUser.email}/>
-        <InformationsUser name={"Rôle"} information={currentUser.role}/>
+        <InformationsUser name={"Nom"} information={currentUser?.getCurrentUser?.firstName}/>
+        <InformationsUser name={"Prénom"} information={currentUser?.getCurrentUser?.lastName}/>
+        <InformationsUser name={"Email"} information={currentUser?.getCurrentUser?.email}/>
+        <InformationsUser name={"Rôle"} information={currentUser?.getCurrentUser?.role}/>
       </View>
     <TaskProgress />
-
     </View>
   );
 }
